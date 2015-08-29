7bcadd67463ff64f8b8cc265b375a5475a713982
damrem <damien.remars@gmail.com>
2015-08-29 17:33:25 +0200

    noise transparency & density

--- src/hxlpers/display/BitmapDataSF.hx
+++ src/hxlpers/display/BitmapDataSF.hx
@@ -13,25 +13,68 @@ import openfl.utils.ByteArray;
 class BitmapDataSF
 {
 
-	static public function simpleNoise(dt:BitmapData, monochromatic:Bool=true)
+	static public function simpleNoise(dt:BitmapData, density:Float=1, monochromatic:Bool=true, transparent:Bool=true)
 	{
 		var w:UInt = cast(dt.width);
 		var h:UInt = cast(dt.height);
 		var pixels:ByteArray = new ByteArray();
-		if (monochromatic)
+		if (monochromatic && !transparent)
 		{
 			for (i in 0...w*h)
 			{
-				pixels.writeUnsignedInt(ColorComponent.OPAQUE + RndColor.gray());
+				if (Rnd.chance(density))
+				{
+					pixels.writeUnsignedInt(ColorComponent.OPAQUE + RndColor.gray());
+				}
+				else
+				{
+					pixels.writeUnsignedInt(0);
+				}
 			}
 		}
-		else
+		else if(!monochromatic && transparent)
 		{
 			for (i in 0...w*h)
 			{
-				pixels.writeUnsignedInt(ColorComponent.OPAQUE + RndColor.RRGGBB());
+				if (Rnd.chance(density))
+				{
+					pixels.writeUnsignedInt(RndColor.AA() + RndColor.RRGGBB());
+				}
+				else
+				{
+					pixels.writeUnsignedInt(0);
+				}
 			}
 		}
+		else if(monochromatic && transparent)
+		{
+			for (i in 0...w*h)
+			{
+				if (Rnd.chance(density))
+				{
+					pixels.writeUnsignedInt(RndColor.AA() + RndColor.gray());
+				}
+				else
+				{
+					pixels.writeUnsignedInt(0);
+				}
+			}
+		}
+		else if(!monochromatic && !transparent)
+		{
+			for (i in 0...w*h)
+			{
+				if (Rnd.chance(density))
+				{
+					pixels.writeUnsignedInt(ColorComponent.OPAQUE + RndColor.RRGGBB());
+				}
+				else
+				{
+					pixels.writeUnsignedInt(0);
+				}
+			}
+		}
+		
 		pixels.position = 0;
 		var zone = new Rectangle(0, 0, w, h);
 		dt.setPixels(zone, pixels);
